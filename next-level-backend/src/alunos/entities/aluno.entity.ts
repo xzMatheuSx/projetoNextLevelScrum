33f822a261b512b78afbc8a3d5b385a29717c581
Leticia Daniele Perotti <80782049+leticia-perotti@users.noreply.github.com>
2025-03-23 13:12:25 -0300

    Ajuste entidade aluno

--- next-level-backend/src/alunos/entities/aluno.entity.ts
+++ next-level-backend/src/alunos/entities/aluno.entity.ts
@@ -1,24 +1,33 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, Unique } from 'typeorm';
 import { Usuario} from 'src/usuarios/entities/usuario.entity';
+import { isString } from 'util';
+import { IsNotEmpty, IsString } from 'class-validator';
 export class Aluno {
 
     @PrimaryGeneratedColumn()
     matricula:number
 
     @Column()
+    @IsNotEmpty()
+    @IsString()
     nome:string
 
-    @Column()
+    @Column({ unique: true })
+    @IsNotEmpty()
+    @IsString()
     cpf: string
 
     @Column()
     email:string
 
     @Column()
-    diaVencimento: string
+    telefone:string
+
+    @Column()
+    diaVencimento: number
 
     
     @ManyToOne(() => Usuario, (usuario) => usuario.id)
-  usuarioAlt: Usuario;
+    usuarioAlt: Usuario;
 
 }
