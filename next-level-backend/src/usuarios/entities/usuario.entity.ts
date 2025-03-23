d6cc1213b7133f6e01d76fc918331e43d414b251
gabriel21rp <gp984358@gmail.com>
2025-03-22 22:10:12 -0300

    populating Usuario entitie

--- next-level-backend/src/usuarios/entities/usuario.entity.ts
+++ next-level-backend/src/usuarios/entities/usuario.entity.ts
@@ -1,1 +1,20 @@
 export class Usuario {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Usuarios {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  nome: string;
+
+  @Column()
+  usuario: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  senha: string;
+}
