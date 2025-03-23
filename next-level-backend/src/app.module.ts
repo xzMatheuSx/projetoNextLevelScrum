ca0397e8b5b0b3acb88356a790e4063d682d47cc
jgodoy88 <joao.godoy7008@gmail.com>
2025-03-22 22:56:04 -0300

    update app module

--- next-level-backend/src/app.module.ts
+++ next-level-backend/src/app.module.ts
@@ -1,13 +1,31 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AlunosModule } from './alunos/alunos.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { ProdutosModule } from './produtos/produtos.module';
 import { EquipamentosModule } from './equipamentos/equipamentos.module';
+import { Usuario } from './usuarios/entities/usuario.entity';
+import { Aluno } from './alunos/entities/aluno.entity';
 
 @Module({
-  imports: [AlunosModule, UsuariosModule, ProdutosModule, EquipamentosModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres', 
+      host: 'localhost', 
+      port: 5432,
+      username: 'root', 
+      password: 'root',
+      database: 'NextLevel', 
+      entities: [Usuario,Aluno],
+      synchronize: true, 
+    }),
+    AlunosModule,
+    UsuariosModule,
+    ProdutosModule,
+    EquipamentosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
