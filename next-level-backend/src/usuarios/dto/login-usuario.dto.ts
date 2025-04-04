937212a4492aedd3f155e06a95c4c7c35ebc7d5f
Leticia Daniele Perotti <80782049+leticia-perotti@users.noreply.github.com>
2025-04-03 23:11:49 -0300

    Ajuste swagger autenticação

--- next-level-backend/src/usuarios/dto/login-usuario.dto.ts
+++ next-level-backend/src/usuarios/dto/login-usuario.dto.ts
@@ -1,5 +1,9 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class LoginUsuarioDTO {
+    @ApiProperty({ description: 'Usuário' })
     usuario: string;
+    @ApiProperty({ description: 'Senha' })
     senha: string;
 
     constructor(usuario: string, senha: string) {
